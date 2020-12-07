65845cb59f7fd44758e1c7bb9d645695a6d2cc14
paumann <paul.aumann@gmail.com>
2020-12-07 02:02:09 +0100

    Added basic functionality

---
+++ Controller.py
@@ -0,0 +1,113 @@
+import os, discord, sys, asyncio
+
+from dotenv import load_dotenv
+from discord.ext import commands
+from Game import Game
+
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+
+bot = commands.Bot(command_prefix="-")
+
+number_emojis = [ "1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣","7️⃣" ]
+games = []
+
+@bot.event
+async def on_ready():
+    print(f"Bot online with id={bot.user.id}.")
+
+@bot.command(name="start", help="Starts a game of connect 4.")
+async def start(ctx):
+    msg = ctx.message
+    author = msg.author
+    channel = msg.channel
+
+    game = Game()
+    game.yellow_player = author
+    game.channel = channel
+    games.append(game)
+
+    await draw_game(game)
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    # Ignore own reactions
+    if user.id == bot.user.id:
+        return
+
+    message = reaction.message
+    game = get_game(message)
+    num = get_input(reaction)
+    # Check if reaction is valid and belongs to a valid game
+    if game != None and num != -1:
+        # Check if its the users turn, and if yes, place the stone
+        if game.is_user_turn(user):
+            game.place(num)
+            if game.check_win():
+                await draw_winscreen(game)
+                try:
+                    del games[games.index(game)]
+                except:
+                    pass
+            else:
+                await draw_game(game)
+        
+
+    await reaction.remove(user)
+
+async def draw_winscreen(game):
+    msg = "Player " + game.get_winner() + " has won! :tada:\n\n"
+    msg += game_string(game)
+
+    await game.message.edit(content=msg)
+    await game.message.clear_reactions()
+
+async def draw_game(game):
+    msg = "Waiting for " + game.get_player() + "'s ("
+    if game.is_red_turn():
+        msg += ":red_circle:"
+    else:
+        msg += ":yellow_circle:"
+    msg += ")move ...\n\n"
+    msg += game_string(game)
+
+    if game.message == None:
+        message = await game.channel.send(msg)
+        game.message = message
+        for emoji in number_emojis:
+            await message.add_reaction(emoji)
+    else:
+        await game.message.edit(content=msg)
+
+
+def game_string(game):
+    string = ""
+    for y in range(game.height):
+        line = ""
+        for x in range(game.width):
+            if game.is_red(x,y):
+                line += ":red_circle:"
+            elif game.is_yellow(x,y):
+                line += ":yellow_circle:"
+            else:
+                line += ":white_circle:"
+            line += " " * 7
+        string += line + "\n"
+    return string
+
+
+def get_game(message):
+    for game in games:
+        if message.id == game.message.id:
+            return game
+    return None
+
+def get_input(reaction):
+    for i in range(len(number_emojis)):
+        if number_emojis[i] == reaction.emoji:
+            return i
+    return -1
+
+
+bot.run(token)
+
---
+++ Game.py
@@ -0,0 +1,142 @@
+Red = "red"
+Yellow = "yellow"
+Empty = None
+
+class Game:
+    def __init__(self, width=7, height=6):
+        self._board = []
+        self._current_player = Yellow
+
+        self.width = width
+        self.height = height
+        self.red_player = None
+        self.yellow_player = None
+        self.message = None
+        self.channel = None
+        self.winner = None
+
+        # Generate the starting board
+        for x in range(width):
+            col = []
+            for y in range(height):
+                col.append(Empty)
+            self._board.append(col)
+    
+    def _next_player(self):
+        self._current_player = Red if self._current_player == Yellow else Yellow
+
+    def get_player(self):
+        if self._current_player == Red:
+            return self.red_player.mention if self.red_player != None else "Red"
+        else:
+            return self.yellow_player.mention if self.yellow_player != None else "Yellow"
+
+    def check_win(self):
+        # Check for vertical wins
+        for x in range(self.width):
+            score = 0
+            last_stone = None
+            for y in range(self.height):
+                stone = self._board[x][y]
+                if stone != last_stone:
+                    score = 1
+                    last_stone = stone
+                elif stone != Empty:
+                    score += 1
+                    if score >= 4:
+                        self.winner = stone
+                        return True
+
+        # Check for horizontal wins
+        for y in range(self.height):
+            score = 0
+            last_stone = None
+            for x in range(self.width):
+                stone = self._board[x][y]
+                if stone != last_stone:
+                    score = 1
+                    last_stone = stone
+                elif stone != Empty:
+                    score += 1
+                    if score >= 4:
+                        self.winner = stone
+                        return True
+
+        # Check for diagonal wins (two different diagonal possibilities)
+        # (1,1,0) -> (delta_x, delta_y, startx when iterating y)
+        for direction in ((1,1,0), (-1,1,self.width-1)):
+            for startx in range(self.width):
+                y = 0
+                x = startx
+                score = 0
+                last_stone = None
+                while x >= 0 and x < self.width and y >= 0 and y < self.height:
+                    stone = self._board[x][y]
+                    if stone != last_stone:
+                        score = 1
+                        last_stone = stone
+                    elif stone != Empty:
+                        score += 1
+                        if score >= 4:
+                            self.winner = stone
+                            return True
+                    x += direction[0]
+                    y += direction[1]
+                
+            for starty in range(self.height):
+                y = starty
+                x = direction[2]
+                score = 0
+                last_stone = None
+                while x >= 0 and x < self.width and y >= 0 and y < self.height:
+                    stone = self._board[x][y]
+                    if stone != last_stone:
+                        score = 1
+                        last_stone = stone
+                    elif stone != Empty:
+                        score += 1
+                        if score >= 4:
+                            self.winner = stone
+                            return True
+                    x += direction[0]
+                    y += direction[1]
+
+        return False
+
+    def place(self, col_nr):
+        col = self._board[col_nr]
+        for i in range(self.height - 1, -1, -1):
+            if (col[i] == Empty):
+                col[i] = self._current_player
+                self._next_player()
+                return True
+        return False
+
+    def is_user_turn(self, user):
+        if self.is_yellow_turn() and self.yellow_player == user:
+            return True
+        elif self.is_red_turn():
+            if self.red_player == None:
+                self.red_player = user
+                return True
+            else:
+                return self.red_player == user
+
+    def is_red(self, x, y):
+        return self._board[x][y] == Red
+
+    def is_yellow(self, x, y):
+        return self._board[x][y] == Yellow
+
+    def is_empty(self, x, y):
+        return self._board[x][y] == Empty
+
+    def is_red_turn(self):
+        return self._current_player == Red
+
+    def is_yellow_turn(self):
+        return self._current_player == Yellow
+
+    def get_winner(self):
+        return self.red_player.mention if self.winner == Red else self.yellow_player.mention
+
